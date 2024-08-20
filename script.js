0e1f4841e098cd7d539106ac62d653c8fd523c13
Tangguhchandra <155598432+Tangguhchandra@users.noreply.github.com>
2024-08-20 15:41:52 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const hamburger = document.querySelector('.hamburger');
-    const menu = document.querySelector('.menu');
-
-    hamburger.addEventListener('click', () => {
-        menu.classList.toggle('active');
-    });
-});
